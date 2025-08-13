7a9604d3959ad15c6fa970f88c0d59531b8fdc40
Icebytesize <Matthiasuk@gmail.com>
2025-08-13 13:50:11 +0200

    Add project files.

---
+++ Biograf billet program/Moduler/Message.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biograf_billet_program.Moduler
+{
+    internal class Message
+    {
+        
+        /// <summary>
+        /// Metode til at skrive tekst til konsollen
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void PrintMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        /// <summary>
+        /// Metode til at udskrive en fejlbesked til konsollen
+        /// </summary>
+        public static void ErrorMessage()
+        {
+            PrintMessage("Input ikke forstået, prøv igen");
+        }
+
+        /// <summary>
+        /// Metode til at udskrive til konsollen hvis man prøver at booke et optaget sæde
+        /// </summary>
+        public static void OptagetPlads()
+        {
+            PrintMessage("Sæde allerede optaget, kan ikke vælges");  
+        }
+
+        /// <summary>
+        /// Metode til at nulstille konsollen
+        /// </summary>
+        public static void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+
+    }
+}
